5109beb05be17f78624c5edc5a7ee02a87623fd3
ianfleming464 <ianflemingdeveloper@gmail.com>
2022-12-05 22:37:36 +0100

    Initial commit: connect to Firebase Firestore, fetch the data
    Backend is ready on firebase. Things displaying in order as required, array sorted prior to display.
    
    TO DO:
    
    Think about file and folder structure, component hierarchy, styling options.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import db from './firebase';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const colRef = collection(db, 'movies'); // connecting to database
+
+  const getMovies = async () => {
+    // function grabs the movies and stores them in state
+    const data = await getDocs(colRef);
+    setMovies(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+  };
+
+  useEffect(() => {
+    getMovies();
+    console.table(movies);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ul>
+        {movies
+          .sort(function (a, b) {
+            return a.year - b.year;
+          })
+          .map(movie => {
+            return <li key={movie.id}>{`${movie.title} : ${movie.description}`}</li>;
+          })}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// TO DO;
+// Backend is finished
+// sort() arranges movies by year. Time to look at displaying. So hierarchy: App->MoviesView (a large responsive card/container)->MovieCard (individual smaller cards, displaying the movies title, poster, shortened description) -> MovieView (click into it, individual movie screen with all the info)
