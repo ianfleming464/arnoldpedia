2de0a021e6a6e892c36cf67e28fe4d76f9c3edb6
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-03-23 13:56:06 +0100

    Added an Arnold SVG

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 function Header() {
   return (
     <AppBar position='fixed'>
       <Toolbar>
-        <FitnessCenterIcon />
+        <StyledIconButton edge='start' color='inherit'>
+          <LogoImg src={require('./assets/image.svg').default} alt='logo' />
+        </StyledIconButton>
         <HeaderTitle variant='h6' component='h1'>
           A R N O L D P E D I A
         </HeaderTitle>
@@ -19,4 +20,18 @@ export default Header;
 
 const HeaderTitle = styled(Typography)({
   marginLeft: 16,
+});
+
+const StyledIconButton = styled(IconButton)({
+  padding: 8,
+  '& .MuiSvgIcon-root': {
+    width: '32px',
+    height: '32px',
+  },
+});
+
+const LogoImg = styled('img')({
+  width: '32px',
+  height: '32px',
+  display: 'block',
 });
