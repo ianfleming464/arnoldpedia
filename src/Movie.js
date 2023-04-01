35df98fca4a98ce67826c1ee64623c050adc27d1
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-04-01 17:46:49 +0200

    Update Movie.js
    Changed text decoration on LearnMore button, made the posters larger and more responsive.

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import MovieView from './MovieView';
 import Card from '@mui/material/Card';
 import { Typography, CardContent, CardActions, CardMedia, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function Movie({ movie }) {
   let { title, year, poster, id } = movie;
+  // const { movieId } = useParams();
 
   return (
     <>
       <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-        <CardMedia component='img' alt='movie poster' src={poster} sx={{ objectFit: 'contain' }} />
+        <CardMedia
+          component='img'
+          alt='movie poster'
+          src={poster}
+          sx={{ objectFit: 'contain', width: '100%', height: '150%' }}
+        />
         <CardContent>
           <Typography gutterBottom variant='h5' component='div' textAlign='center'>
             {title}
@@ -26,8 +33,19 @@ function Movie({ movie }) {
               display: 'flex',
               justifyContent: 'space-between',
             }}>
-            <Button sx={{ mt: 3, ml: 1 }} variant='contained  '>
-              <Link to={`${id}`} state={movie}>
+            <Button sx={{ mt: 3, ml: 1, textDecoration: 'none' }} variant='contained'>
+              <Link
+                to={`${id}`}
+                state={movie}
+                style={{
+                  textDecoration: 'none',
+                  color: 'inherit',
+                  '&:hover': {
+                    textDecoration: 'none',
+                    color: 'inherit',
+                  },
+                }}>
+                {' '}
                 Learn more
               </Link>
             </Button>
