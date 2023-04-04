eea1799770b6ca527efbe2c72b24e490454af765
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-04-04 19:28:19 +0200

    Update theme.js
    Added a rolling to-do list

--- src/theme.js
+++ src/theme.js
@@ -17,3 +17,10 @@ export const appTheme = createTheme({
     fontWeightBold: 700,
   },
 });
+
+// TO DO: 04.04
+// In Firebase, shorten the synopsis and add an in-film Arnold action photo for EACH movie. Then,
+// in the MovieView, we could have it as a background to contrast the poster in the foreground.
+// Check the colour scheme! Because it's pretty shite atm.
+// Homepage ideas?? A brief Arnold bio and links to his new pod, books, website etc. Perhaps an
+// image caraousel of the big man.
