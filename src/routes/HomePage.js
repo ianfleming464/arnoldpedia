dc769db4f5e6473203cdc24c666863bc434c9462
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-04-18 15:10:33 +0200

    Update HomePage.js

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { ref, getDownloadURL, getStorage } from 'firebase/storage';
-import { Typography, Box, Button, Card, CardMedia, Grid } from '@mui/material';
+import { Typography, Box, Button, Card, CardMedia, CardContent, Grid } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { auth, logout } from '../firebase';
 
@@ -35,40 +35,44 @@ function HomePage() {
   return (
     <>
       <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
-        <Card
-          sx={{
-            width: '50%',
-            p: 5,
-            m: 5,
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
-            opacity: isLoaded ? 1 : 0,
-            transition: 'opacity 1s ease',
-          }}>
-          <CardMedia
-            component='img'
-            src={imageUrl}
-            alt='Arnold'
-            onLoad={handleImageLoad}
-            sx={{
-              objectFit: 'contain',
-              height: '100%',
-              width: '100%',
-            }}
-          />
+        <Card sx={{ width: '75%', p: 2, m: 2, mt: 10 }}>
+          <Box display='flex' flexDirection='row' justifyContent='space-between'>
+            <CardMedia
+              component='img'
+              src={imageUrl}
+              alt='Arnold'
+              onLoad={handleImageLoad}
+              sx={{
+                objectFit: 'contain',
+                height: '100%',
+                width: '50%',
+              }}
+            />
+            <CardContent sx={{ width: '50%', mt: 5 }}>
+              <Typography variant='body1' component='p'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod lorem at felis
+                blandit, ac sagittis elit bibendum. Vivamus ac ullamcorper purus, quis tempor magna.
+                Nullam lobortis nulla vitae massa maximus, sed imperdiet mauris mollis. Proin
+                vehicula feugiat arcu eget vestibulum. Fusce fringilla nisi vitae odio auctor, eget
+                ullamcorper erat laoreet. In hac habitasse platea dictumst. Sed euismod mi a lacus
+                dapibus, a pulvinar eros fermentum. Maecenas non metus non odio vestibulum dapibus a
+                id dolor. Donec vitae nunc sit amet lectus eleifend sagittis eu eget augue.
+              </Typography>
+            </CardContent>
+          </Box>
+          <Grid container justifyContent='center' sx={{ mt: 2 }}>
+            <Grid item>
+              <Button variant='contained' sx={{ m: 1 }}>
+                <Link to='movies'>Arnold's Movies</Link>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant='contained' onClick={logout} sx={{ m: 1 }}>
+                Logout
+              </Button>
+            </Grid>
+          </Grid>
         </Card>
-
-        <Grid container spacing={2} justifyContent='center'>
-          <Grid item>
-            <Button variant='contained' sx={{ m: 1 }}>
-              <Link to='movies'>Arnold's Movies</Link>
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button variant='contained' onClick={logout} sx={{ m: 1 }}>
-              Logout
-            </Button>
-          </Grid>
-        </Grid>
       </Box>
     </>
   );
